87e0b738e75175133aa6ae7285efceade75eb46e
Mike Klein <mtklein@gmail.com>
2023-07-06 11:33:49 -0400

    rearrange

--- test.c
+++ test.c
@@ -14,12 +14,11 @@ int main(void) {
     int const arr[] = { 1,3,7,9,13,17,23,41,99,1004 };
 
     for (int i = 0; i < len(arr); i++) {
-        expect(bsearch_int_const(arr,len(arr), arr+i, lt_int_const, (void*)0) == arr+i);
-
         int const lo = arr[i] - 1,
                   hi = arr[i] + 1;
-        expect(bsearch_int_const(arr,len(arr), &lo, lt_int_const, (void*)0) == arr+i  );
-        expect(bsearch_int_const(arr,len(arr), &hi, lt_int_const, (void*)0) == arr+i+1);
+        expect(bsearch_int_const(arr,len(arr), arr+i, lt_int_const, (void*)0) == arr+i  );
+        expect(bsearch_int_const(arr,len(arr),   &lo, lt_int_const, (void*)0) == arr+i  );
+        expect(bsearch_int_const(arr,len(arr),   &hi, lt_int_const, (void*)0) == arr+i+1);
     }
 
     return 0;
